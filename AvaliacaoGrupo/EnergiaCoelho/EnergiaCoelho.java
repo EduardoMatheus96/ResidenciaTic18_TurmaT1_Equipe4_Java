07c63769b7dd29205750cd4ecad9f9040f7e099f
Víctor Quirino <victorsouzaq@gmail.com>
2023-12-05 16:55:56 -0300

    Classes Pagamento e Reembolso e gestão de pagamentos

--- AvaliacaoGrupo/EnergiaCoelho/EnergiaCoelho.java
+++ AvaliacaoGrupo/EnergiaCoelho/EnergiaCoelho.java
@@ -1,7 +1,5 @@
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 class Cliente {
 	private String nome;
@@ -106,12 +104,14 @@ class Imovel {
 }
 
 class Fatura {
-	Imovel imovel;
-	double ultimaLeitura;
-	double penultimaLeitura;
-	Date dataEmissao;
-	double valorCalculado;
-	boolean quitada;
+	private Long id;
+	private Imovel imovel;
+	private double ultimaLeitura;
+	private double penultimaLeitura;
+	private Date dataEmissao;
+	private double valorCalculado;
+	private boolean quitada;
+	private List<Pagamento> pagamentos;
 
 	@Override
 	public String toString() {
@@ -120,22 +120,83 @@ class Fatura {
 				+ "\nValor Calculado: " + valorCalculado + "\nQuitada: " + quitada + "\n---------------------";
 	}
 
-	public Fatura(Imovel imovel, double ultimaLeitura, double penultimaLeitura) {
+	public Fatura(Long id, Imovel imovel, double ultimaLeitura, double penultimaLeitura) {
+		this.id = id;
 		this.imovel = imovel;
 		this.ultimaLeitura = ultimaLeitura;
 		this.penultimaLeitura = penultimaLeitura;
 		this.dataEmissao = new Date();
 		this.valorCalculado = calcularValor();
 		this.quitada = false;
+		pagamentos = new ArrayList<Pagamento>();
 	}
 
 	private double calcularValor() {
 		// Admitindo um custo de 10 reais por KWh
 		return (ultimaLeitura - penultimaLeitura) * 10;
 	}
+
+	public void adicionaPagamento(Pagamento pagamento){
+		if(quitada){
+			System.out.println("A fatura ja esta quitada! Nao foi possivel adicionar um pagamento.");
+			return;
+		}
+
+		pagamentos.add(pagamento);
+		if(valorPago() >= calcularValor()){
+			quitada = true;
+			if(valorPago() > calcularValor()){
+				pagamento.geraReembolso(valorPago() - valorCalculado);
+				System.out.printf("O valor total pago ultrapassou o valor da fatura, foi gerado um reembolso no valor de R$%.2f%n", pagamento.getReembolso().getValor());
+			}
+		}
+	}
+
+	public double valorPago(){
+		double valor = 0.0f;
+		for (var pagamento : pagamentos){
+			valor += pagamento.getValor();
+		}
+
+		return valor;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public Imovel getImovel() {
+		return imovel;
+	}
+
+	public double getUltimaLeitura() {
+		return ultimaLeitura;
+	}
+
+	public double getPenultimaLeitura() {
+		return penultimaLeitura;
+	}
+
+	public Date getDataEmissao() {
+		return dataEmissao;
+	}
+
+	public double getValorCalculado() {
+		return valorCalculado;
+	}
+
+	public boolean isQuitada() {
+		return quitada;
+	}
+
+	public List<Pagamento> getPagamentos() {
+		return pagamentos;
+	}
 }
 
 public class EnergiaCoelho {
+	private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	private static Long idsFatura = 0L;
 	public static ArrayList<Cliente> getClientes() {
 		return clientes;
 	}
@@ -181,6 +242,16 @@ public class EnergiaCoelho {
 				return cliente;
 			}
 		}
+		return null;
+	}
+
+	static Fatura buscarFaturaPorId(Long id){
+		for(var fatura : faturas){
+			if(fatura.getId().equals(id)){
+				return fatura;
+			}
+		}
+
 		return null;
 	}
 
@@ -399,8 +470,9 @@ public class EnergiaCoelho {
 			int ultimaLeitura = imovel.getUltimaLeitura();
 			int penultimaLeitura = imovel.getPenultimaLeitura();
 
-			Fatura novaFatura = new Fatura(imovel, leituraAtual, penultimaLeitura);
+			Fatura novaFatura = new Fatura(idsFatura, imovel, leituraAtual, penultimaLeitura);
 			faturas.add(novaFatura);
+			idsFatura++;
 
 			// Atualiza a última leitura no imóvel
 			imovel.setPenultimaLeitura(ultimaLeitura);
@@ -415,11 +487,11 @@ public class EnergiaCoelho {
 	static void listarFaturasAbertas() {
 		System.out.println("Faturas Abertas (Não Pagas):");
 		for (Fatura fatura : faturas) {
-			if (!fatura.quitada) {
-				System.out.println("Matrícula: " + fatura.imovel.getMatricula());
-				System.out.println("Valor: " + fatura.valorCalculado);
-				System.out.println("Data de Emissão: " + fatura.dataEmissao);
-				System.out.println("Quitada: " + fatura.quitada);
+			if (!fatura.isQuitada()) {
+				System.out.println("Matrícula: " + fatura.getImovel().getMatricula());
+				System.out.println("Valor: " + fatura.getValorCalculado());
+				System.out.println("Data de Emissão: " + fatura.getDataEmissao());
+				System.out.println("Quitada: " + fatura.isQuitada());
 				System.out.println("------");
 			}
 		}
@@ -428,11 +500,121 @@ public class EnergiaCoelho {
 	static void listarTodasFaturas() {
 		System.out.println("Todas as Faturas:");
 		for (Fatura fatura : faturas) {
-			System.out.println("Matrícula: " + fatura.imovel.getMatricula());
-			System.out.println("Valor: " + fatura.valorCalculado);
-			System.out.println("Data de Emissão: " + fatura.dataEmissao);
-			System.out.println("Quitada: " + fatura.quitada);
+			System.out.println("Id: " + fatura.getId());
+			System.out.println("Matrícula: " + fatura.getImovel().getMatricula());
+			System.out.println("Valor: " + fatura.getValorCalculado());
+			System.out.println("Data de Emissão: " + fatura.getDataEmissao());
+			System.out.println("Quitada: " + fatura.isQuitada());
 			System.out.println("------");
+		}
+	}
+
+	static void incluirPagamento(){
+		System.out.println("Informe o CPF do cliente: ");
+		String cpfCliente = scanner.next();
+
+		Cliente cliente = buscarClientePorCPF(cpfCliente);
+
+		if (cliente == null) {
+			System.out.println("Cliente não encontrado para efetuar pagamento.");
+			return;
+		}
+
+		System.out.printf("Digite a matrícula do imovel de %s para gerar o pagamento da fatura:%n", cliente.getNome());
+		int matricula = scanner.nextInt();;
+		for(var imovel : cliente.getImoveis()){
+			if(imovel.getMatricula() == matricula){
+				System.out.println("Digite o valor do pagamento:");
+				double valor = scanner.nextDouble();
+				var pagamento = new Pagamento(valor);
+
+				for(var fatura : faturas){
+					if(fatura.getImovel().equals(imovel)){
+						fatura.adicionaPagamento(pagamento);
+						System.out.println("Pagamento efetuado com sucesso!");
+						return;
+					}
+				}
+			}
+		}
+	}
+
+	static void listarPagamentos(){
+		System.out.println("Lista de pagamentos:");
+		int i = 1;
+		for(var fatura : faturas){
+			System.out.println("--------------");
+			System.out.println("Fatura nº: " + i);
+			System.out.println("Imovel: " + fatura.getImovel());
+			System.out.println("Pagamentos");
+			for(var pagamento : fatura.getPagamentos()){
+				System.out.println("---------------");
+				System.out.println("Data: " + sdf.format(pagamento.getData()));
+				System.out.printf("Valor: R$%.2f%n", pagamento.getValor());
+				if(pagamento.getReembolso() != null){
+					System.out.println("Reembolso:");
+					System.out.println("Data: " + pagamento.getReembolso().getData());
+					System.out.printf("Valor: R$%.2f%n", pagamento.getReembolso().getValor());
+				}
+			}
+		}
+		System.out.println("---------------");
+	}
+
+	static void listarPagamentosDeFatura(){
+		listarTodasFaturas();
+		System.out.println("\nDigite o id da fatura que deseja listar os pagamentos:");
+		Long idFatura = scanner.nextLong();
+		var fatura = buscarFaturaPorId(idFatura);
+
+		if(fatura != null){
+			System.out.printf("Pagamentos da fatura de id %d:%n", fatura.getId());
+			for(var pagamento : fatura.getPagamentos()){
+				System.out.println("Data: " + sdf.format(pagamento.getData()));
+				System.out.printf("Valor: R$%.2f%n", pagamento.getValor());
+			}
+			System.out.println("---------------");
+		} else {
+			System.out.println("Nao existe uma fatura com esse id!");
+		}
+	}
+
+	static void listarReembolsos(){
+		System.out.println("Lista de reembolsos:");
+		for(var fatura : faturas){
+			if(!fatura.getPagamentos().isEmpty()){
+				for(var pagamento : fatura.getPagamentos()){
+					if(pagamento.getReembolso() != null){
+						System.out.println("Data: " + sdf.format(pagamento.getReembolso().getData()));
+						System.out.printf("Valor: R$%.2f%n", pagamento.getReembolso().getValor());
+					}
+				}
+			}
+		}
+	}
+
+	static void exibirReembolsoDeFatura(){
+		listarTodasFaturas();
+		System.out.println("\nDigite o id da fatura que deseja exibir o reembolso:");
+		Long idFatura = scanner.nextLong();
+		var fatura = buscarFaturaPorId(idFatura);
+
+		if(fatura != null){
+			if(!fatura.getPagamentos().isEmpty()){
+				for(var pagamento : fatura.getPagamentos()){
+					if(pagamento.getReembolso() != null){
+						System.out.println("Reembolso:");
+						System.out.println("Data: " + sdf.format(pagamento.getReembolso().getData()));
+						System.out.printf("Valor: R$%.2f%n", pagamento.getReembolso().getValor());
+						return;
+					}
+				}
+				System.out.println("Nao existe reembolsos relacionados a essa fatura!");
+			} else {
+				System.out.println("Nao existe pagamentos relacionados a essa fatura, portanto nao ha reembolso!");
+			}
+		} else {
+			System.out.println("Nao existe uma fatura com esse id!");
 		}
 	}
 
@@ -443,6 +625,7 @@ public class EnergiaCoelho {
 			System.out.println("1. Gestão de Clientes");
 			System.out.println("2. Gestão de Imóveis");
 			System.out.println("3. Gestão de Faturas");
+			System.out.println("4. Gestão de Pagamentos");
 			System.out.println("0. Sair");
 			System.out.print("Escolha uma opção: ");
 			opcao = scanner.nextInt();
@@ -456,6 +639,9 @@ public class EnergiaCoelho {
 				break;
 			case 3:
 				menuFaturas();
+				break;
+			case 4:
+				menuPagamentos();
 				break;
 			case 0:
 				System.out.println("Programa Encerrado.");
@@ -577,5 +763,43 @@ public class EnergiaCoelho {
 				break;
 			}
 		} while (opcao != 0);
+	}
+
+	private static void menuPagamentos(){
+		int opcao;
+		do {
+			System.out.println("\n--------Menu Pagamentos--------");
+			System.out.println("1. Incluir pagamento");
+			System.out.println("2. Listar pagamentos");
+			System.out.println("3. Listar pagamentos de uma fatura");
+			System.out.println("4. Listar reembolsos");
+			System.out.println("5. Exibir reembolso de uma fatura");
+			System.out.println("0. Voltar ao menu anterior");
+			System.out.print("\nDigite uma opção: ");
+			opcao = scanner.nextInt();
+
+			switch (opcao){
+				case 1:
+					incluirPagamento();
+					break;
+				case 2:
+					listarPagamentos();
+					break;
+				case 3:
+					listarPagamentosDeFatura();
+					break;
+				case 4:
+					listarReembolsos();
+					break;
+				case 5:
+					exibirReembolsoDeFatura();
+					break;
+				case 0:
+					break;
+				default:
+					System.out.println("Opcao invalida! Tente novamente");
+					break;
+			}
+		}  while(opcao !=  0);
 	}
 }
---
+++ AvaliacaoGrupo/EnergiaCoelho/Reembolso.java
@@ -0,0 +1,19 @@
+import java.util.Date;
+
+public class Reembolso {
+    private Date data;
+    private double valor;
+
+    public Reembolso(double valor) {
+        data = new Date();
+        this.valor= valor;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
