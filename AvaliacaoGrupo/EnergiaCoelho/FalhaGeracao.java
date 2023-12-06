2e16a3942fd3b5fd438d9ddc8116bcbcd1aa3ee3
EduardoMatheus96 <eduardomatheusuesc@gmail.com>
2023-12-06 00:46:57 -0300

    Implementando classes de falhas e Reparo

---
+++ AvaliacaoGrupo/EnergiaCoelho/FalhaGeracao.java
@@ -0,0 +1,7 @@
+import java.time.LocalDate;
+
+public class FalhaGeracao extends Falha {
+    public FalhaGeracao(Imovel imovel, String descricao, LocalDate previsao) {
+        super(imovel, descricao, previsao);
+    }
+}
