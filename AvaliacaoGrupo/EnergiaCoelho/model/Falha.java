d2b42f77f2e3cd36d0192f46f1a741bd2909445d
Lorena Andrade <lorenaandrade@gmail.com>
2023-12-06 15:12:52 -0300

    Ultima leitura nao deve ser menor que penultima leitura

---
+++ AvaliacaoGrupo/EnergiaCoelho/model/Falha.java
@@ -0,0 +1,56 @@
+package model;
+import java.time.LocalDate;
+
+public class Falha {
+    private Imovel imovel;
+    private String descricao;
+    private LocalDate previsao;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+
+    public Falha(Imovel imovel, String descricao, LocalDate previsao) {
+        this.setImovel(imovel);
+        this.setDescricao(descricao);
+        this.setPrevisao(previsao);
+    }
+
+	public Imovel getImovel() {
+		return imovel;
+	}
+
+	public void setImovel(Imovel imovel) {
+		this.imovel = imovel;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public LocalDate getDataInicio() {
+		return dataInicio;
+	}
+
+	public void setDataInicio(LocalDate dataInicio) {
+		this.dataInicio = dataInicio;
+	}
+
+	public LocalDate getPrevisao() {
+		return previsao;
+	}
+
+	public void setPrevisao(LocalDate previsao) {
+		this.previsao = previsao;
+	}
+
+	public LocalDate getDataFim() {
+		return dataFim;
+	}
+
+	public void setDataFim(LocalDate dataFim) {
+		this.dataFim = dataFim;
+	}
+}
